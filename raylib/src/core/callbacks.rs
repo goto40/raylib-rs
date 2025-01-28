0f5e409feb7ff72ae1cf536c6b310adcc782fc6f
goto40 <pierre.bayerl@googlemail.com>
2025-01-28 19:37:32 +0100

    fn to Fn

--- raylib/src/core/callbacks.rs
+++ raylib/src/core/callbacks.rs
@@ -265,7 +265,7 @@ impl<'a> Drop for AudioStreamProcessor<'a> {
 
 pub fn attach_audio_stream_processor_to_music<'a>(
     music: &'a Music<'a>,
-    processor: Fn(usize, &[f32])->(),
+    processor: Box<dyn Fn(usize, &[f32]) -> () + Send + Sync>,
 ) -> AudioStreamProcessor<'a> {
     let nb_channels_from_music = music.stream.channels as usize;
     let my_closure = Box::new(move |data_ptr: *mut c_void, frames: u32| -> () {
